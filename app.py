ecf6579aee1d87a122754050f3fab37130cba38f
tonykurya <tonykurya@gmail.com>
2017-05-04 17:40:42 +0300

    Implemented the send_message function

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def verify():
 
     return "Hello world", 200
 
+
 @app.route('/', methods=['POST'])
 def webhook():
 
@@ -50,5 +51,29 @@ def webhook():
                     pass
 
     return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
