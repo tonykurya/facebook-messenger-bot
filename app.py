c9efd2259e47f1d6024dc24171fb9fc02ced1a09
tonykurya <tonykurya@gmail.com>
2017-05-04 17:43:09 +0300

    Added a simple wrapper for logging to stdout on heroku

--- app.py
+++ app.py
@@ -77,3 +77,12 @@ def send_message(recipient_id, message_text):
         log(r.text)
 
 
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
